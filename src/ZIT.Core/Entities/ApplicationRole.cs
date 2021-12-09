29bbe2525628bcbc85799d79fef1837d6376f7ad
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-12-09 20:23:29 +0100

    #3 Add auth models

---
+++ src/ZIT.Core/Entities/ApplicationRole.cs
@@ -0,0 +1,23 @@
+﻿using ZIT.Core.Common;
+
+namespace ZIT.Core.Entities;
+
+public class ApplicationRole : AuditableEntity
+{
+    public string Name { get; set; }
+    public ICollection<string> Entitlements { get; set; }
+
+    public ApplicationRole(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+        Entitlements = Array.Empty<string>();
+    }
+
+    public ApplicationRole(Guid id, string name, ICollection<string> entitlements)
+    {
+        Id = id;
+        Name = name;
+        Entitlements = entitlements;
+    }
+}
