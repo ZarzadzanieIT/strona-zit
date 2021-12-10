35e09a25edf5214e2039cb35665c9d4215bb7b9e
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-12-10 23:24:41 +0100

    #10 Add RequireEntitlement attribute with OR between multiple entitlements

---
+++ src/ZIT.Web/Infrastructure/RequireOneEntitlementAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ZIT.Web.Infrastructure;
+
+public class RequireOneEntitlementAttribute : TypeFilterAttribute
+{
+    public RequireOneEntitlementAttribute(string claimType, params string[] claimValues) : base(typeof(RequireOneEntitlementFilter))
+    {
+        Arguments = claimValues.Select(x => new Claim(claimType, x)).ToArray();
+    }
+}
---
+++ src/ZIT.Web/Infrastructure/RequireOneEntitlementFilter.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ZIT.Web.Infrastructure;
+
+public class RequireOneEntitlementFilter : IAuthorizationFilter
+{
+    private readonly Claim[] _claims;
+
+    public RequireOneEntitlementFilter(Claim[] claims)
+    {
+        _claims = claims;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var hasClaim =
+            context.HttpContext?.User?.Claims?.Any(x => _claims.Any(y => y.Type == x.Type && y.Value == x.Value));
+        if (hasClaim == null || !hasClaim.Value)
+        {
+            context.Result = new ForbidResult();
+        }
+    }
+}
