0fc1179bb9bc25eb80495c6b282712f0f2ce9cea
krzysztofzajaczkowski <krzysztof.m.zajaczkowski@gmail.com>
2021-12-16 23:02:35 +0100

    #23 Add SQLite initial migration

--- src/ZIT.Infrastructure/InfrastructureInstaller.cs
+++ src/ZIT.Infrastructure/InfrastructureInstaller.cs
@@ -13,23 +13,8 @@ public static class InfrastructureInstaller
 {
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        var databaseOptions = configuration.Get<DatabaseOptions>();
 
-        services.AddDbContext<AppDbContext>(builder =>
-        {
-            switch (databaseOptions.DatabaseProvider)
-            {
-                case DatabaseProvider.InMemory:
-                    builder.UseInMemoryDatabase("ZIT");
-                    break;
-                case DatabaseProvider.Sqlite:
-                    builder.UseSqlite(configuration.GetConnectionString("Sqlite"));
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(databaseOptions), databaseOptions,
-                        "Unknown database provider");
-            }
-        });
+        services.AddDbContext<AppDbContext>(configuration.GetDbContextOptions());
 
         services.AddScoped<IUserService, UserService>();
         services.AddScoped<IAuthService>(provider => (provider.GetRequiredService<IUserService>() as IAuthService)!);
@@ -39,5 +24,26 @@ public static class InfrastructureInstaller
         services.AddTransient<DataSeeder>();
 
         return services;
+    }
+
+    internal static Action<DbContextOptionsBuilder> GetDbContextOptions(this IConfiguration configuration)
+    {
+        var databaseOptions = configuration.Get<DatabaseOptions>();
+
+        return builder =>
+        {
+            switch (databaseOptions.DatabaseProvider)
+            {
+                case DatabaseProvider.InMemory:
+                    builder.UseInMemoryDatabase("ZIT");
+                    break;
+                case DatabaseProvider.SQLite:
+                    builder.UseSqlite(configuration.GetConnectionString("SQLite"));
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException(nameof(databaseOptions), databaseOptions,
+                        "Unknown database provider");
+            }
+        };
     }
 }
---
+++ src/ZIT.Infrastructure/Persistence/Migrations/SQLite/20211216215928_SQLite_InitialDomain.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZIT.Infrastructure.Persistence.Migrations.SQLite
+{
+    public partial class SQLite_InitialDomain : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Members",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    DepartmentId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Surname = table.Column<string>(type: "TEXT", nullable: true),
+                    PhotoAddress = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Members", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    SlugTitle = table.Column<string>(type: "TEXT", nullable: true),
+                    Summary = table.Column<string>(type: "TEXT", nullable: true),
+                    Content = table.Column<string>(type: "TEXT", nullable: true),
+                    Tags = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Entitlements = table.Column<string>(type: "TEXT", nullable: true),
+                    ParentRoleId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Roles_Roles_ParentRoleId",
+                        column: x => x.ParentRoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SliderElements",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Title = table.Column<string>(type: "TEXT", nullable: true),
+                    ImageAddress = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SliderElements", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Email = table.Column<string>(type: "TEXT", nullable: true),
+                    Password = table.Column<string>(type: "TEXT", nullable: true),
+                    Entitlements = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Departments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    CoordinatorId = table.Column<Guid>(type: "TEXT", nullable: true),
+                    ImageAddress = table.Column<string>(type: "TEXT", nullable: true),
+                    Description = table.Column<string>(type: "TEXT", nullable: true),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    CreatedBy = table.Column<string>(type: "TEXT", nullable: true),
+                    ModifiedAt = table.Column<DateTimeOffset>(type: "TEXT", nullable: true),
+                    ModifiedBy = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Departments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Departments_Members_CoordinatorId",
+                        column: x => x.CoordinatorId,
+                        principalTable: "Members",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ApplicationRoleApplicationUser",
+                columns: table => new
+                {
+                    RolesId = table.Column<Guid>(type: "TEXT", nullable: false),
+                    UsersInRoleId = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ApplicationRoleApplicationUser", x => new { x.RolesId, x.UsersInRoleId });
+                    table.ForeignKey(
+                        name: "FK_ApplicationRoleApplicationUser_Roles_RolesId",
+                        column: x => x.RolesId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ApplicationRoleApplicationUser_Users_UsersInRoleId",
+                        column: x => x.UsersInRoleId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ApplicationRoleApplicationUser_UsersInRoleId",
+                table: "ApplicationRoleApplicationUser",
+                column: "UsersInRoleId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Departments_CoordinatorId",
+                table: "Departments",
+                column: "CoordinatorId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Roles_ParentRoleId",
+                table: "Roles",
+                column: "ParentRoleId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ApplicationRoleApplicationUser");
+
+            migrationBuilder.DropTable(
+                name: "Departments");
+
+            migrationBuilder.DropTable(
+                name: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "SliderElements");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Members");
+        }
+    }
+}
